851d1ed8d2940a3fef681fa603d0dd28945e1396
Patrick Schadler <patrick.schadler@gmail.com>
2020-05-22 13:06:52 +0200

    feat(get-in-touch): add mailto

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import classes from './Button.module.css';
 
-const button = (props) => (
-  <button
-    className={[classes.Button, classes[props.buttonType]].join(' ')}
-    onClick={props.clicked}
-  >
-    {props.children}
-  </button>
-);
+const button = (props) => {
+  let size = 0.9;
+
+  if (props.size) {
+    size = props.size;
+  }
+
+  return (
+    <button
+      className={classes.Button}
+      style={{ fontSize: size + 'rem' }}
+      onClick={props.clicked}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default button;
--- src/components/UI/Introduction/Introduction.js
+++ src/components/UI/Introduction/Introduction.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import classes from './Introduction.module.css';
-import Aux from '../../../hoc/Aux';
 import BigText from '../BigText/BigText';
+import Button from '../Button/Button';
 
-const contentSection = (props) => (
-  <div className={classes.Introduction}>
-    <div className={classes.Heading}>Hi, my name is</div>
-    <BigText>Patrick Schadler</BigText>
-    <BigText color='Dark'>and this is my story.</BigText>
-    <div className={classes.Description}>
-      I am a senior software engineer based in Leibnitz, Austria specialized in
-      building exceptional software products, applications, services and
-      everything in between.
-    </div>
-  </div>
-);
+class Introduction extends React.Component {
+  mailtoHandler = () => {
+    const email = 'patrick.schadler@gmail.com';
+    const subject = 'subject=Hi Patrick';
+    const body = 'body=Hi Patrick, I just saw your portfolio and ....';
 
-export default contentSection;
+    const mailToText = [subject, body].join('&');
+
+    window.location = `mailto:${email}?${mailToText}`;
+  };
+
+  render() {
+    return (
+      <div className={classes.Introduction}>
+        <div className={classes.Heading}>Hi, my name is</div>
+        <BigText>Patrick Schadler</BigText>
+        <BigText color='Dark'>and this is my story.</BigText>
+        <div className={classes.Description}>
+          I am a senior software engineer based in Leibnitz, Austria specialized
+          in building exceptional software products, applications, services and
+          everything in between.
+        </div>
+        <Button clicked={() => this.mailtoHandler()} size={1.5}>
+          Get in touch
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default Introduction;
