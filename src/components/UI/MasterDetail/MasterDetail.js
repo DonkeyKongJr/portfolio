4e9d83926c7845a4ab3060758c78ee7aac9a4269
Patrick Schadler <patrick.schadler@gmail.com>
2020-05-23 20:29:55 +0200

    feat(jobs): add jobs as json file

--- src/components/UI/MasterDetail/MasterDetail.js
+++ src/components/UI/MasterDetail/MasterDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Detail from './Detail/Detail';
 import classes from './MasterDetail.module.css';
+import jobs from './../../../data/jobs.json';
 
 class MasterDetail extends Component {
   constructor(props) {
@@ -8,32 +9,12 @@ class MasterDetail extends Component {
     this.state = { selectedJob: 0 };
   }
 
-  demoJob1 = {
-    id: 0,
-    jobTitle: 'Software Engineer',
-    company: 'Company 1',
-    from: '2018',
-    till: 'present',
-    description: 'I have done awesome stuff.',
-  };
-
-  demoJob2 = {
-    id: 1,
-    jobTitle: 'Software Engineer',
-    company: 'Company 2',
-    from: '2015',
-    till: '2018',
-    description: 'I have done amazing stuff.',
-  };
-
-  jobs = [this.demoJob1, this.demoJob2];
-
   selectJobHandler = (id) => {
     this.setState({ selectedJob: id });
   };
 
   render() {
-    const companyList = this.jobs.map((job) => {
+    const companyList = jobs.map((job) => {
       const listClass = job.id === this.state.selectedJob ? classes.Active : '';
       return (
         <li className={listClass} onClick={() => this.selectJobHandler(job.id)}>
@@ -46,7 +27,7 @@ class MasterDetail extends Component {
       <div>
         <ul className={classes.SelectList}>{companyList}</ul>
 
-        <Detail job={this.jobs[this.state.selectedJob]} />
+        <Detail job={jobs[this.state.selectedJob]} />
       </div>
     );
   }
