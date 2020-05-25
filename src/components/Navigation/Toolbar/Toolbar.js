8788eea85a9a7ead549dad41bd08c0d436c56fce
Patrick Schadler <patrick.schadler@gmail.com>
2020-05-25 09:05:10 +0200

    feat(cv): add link to resume

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,17 +5,26 @@ import Logo from '../../Logo/Logo';
 import Button from '../../UI/Button/Button';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <div className={classes.Logo}>
-      <Logo />
-    </div>
-    <DrawerToggle clicked={props.openMenu} />
-    <nav className={classes.DesktopOnly}>
-      <NavigationItems clicked={props.clicked} />
-      <Button>RESUME</Button>
-    </nav>
-  </header>
-);
+const toolbar = (props) => {
+  const resumeHandler = () => {
+    window.open(
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-bb87c.appspot.com/o/Resume%2FPatrick%20Schadler%20Resume.pdf?alt=media&token=49ef88b2-8916-4146-b08c-313b652798b8',
+      '_blank'
+    );
+  };
+
+  return (
+    <header className={classes.Toolbar}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <DrawerToggle clicked={props.openMenu} />
+      <nav className={classes.DesktopOnly}>
+        <NavigationItems clicked={props.clicked} />
+        <Button clicked={resumeHandler}>RESUME</Button>
+      </nav>
+    </header>
+  );
+};
 
 export default toolbar;
