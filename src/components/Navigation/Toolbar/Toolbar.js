2c4b3cb0df703ad364da23ad8dbdacdeae431b97
Schadler Patrick <patrick.schadler@acp.at>
2020-09-09 14:25:29 +0200

    update experience section

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,11 +8,7 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = (props) => {
   const resumeHandler = () => {
     window.open(
-<<<<<<< HEAD
       'https://firebasestorage.googleapis.com/v0/b/portfolio-bb87c.appspot.com/o/Resume%2FPatrick%20Schadler%20Resume.pdf?alt=media&token=a6d27a3a-8d6d-4763-9ea6-af44e571fb1d',
-=======
-      'https://firebasestorage.googleapis.com/v0/b/portfolio-bb87c.appspot.com/o/Resume%2FPatrick%20Schadler%20Resume.pdf?alt=media&token=ccf1956d-034e-451b-ab80-0e4cef52120d',
->>>>>>> c16d7d3bdac83807d90254611875bb49b5f5eaf3
       '_blank'
     );
   };
